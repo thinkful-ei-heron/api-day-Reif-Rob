4bb15c15ea139f2c8ebebbafc818cb120f6421f2
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-09-26 11:56:33 -0500

    Finished migrating to Webpack
    Changes to be committed:
    new file:   package-lock.json
    new file:   package.json
    renamed:    index.html -> src/index.html
    modified:   src/index.js
    modified:   src/shopping-list.js
    modified:   src/store.js
    new file:   webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
